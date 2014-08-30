034d7827f8ff258ddbf844987c7bb1ff2233178b
Heikki Pesonen <heikki.pesonen@luotta.fi>
2014-08-30 08:59:47 +0300

    restart

--- app/scripts/controllers/menuController.js
+++
@@ -1,7 +0,0 @@
-kysely.controller('menuController', ['$scope','$location','$element', function($scope, $location, $element){
-	
-	$scope.currentPath = $location.url();
-	$scope.$on('$routeChangeStart',function(){
-		$scope.currentPath = $location.url();
-	});
-}])
